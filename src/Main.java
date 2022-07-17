2157e41a7a00067c756881886f6b460b78539271
Danem <Danem22mail.ru>
2022-07-17 19:48:36 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
         int eaters = 5; // сколько людей будут есть
 
-        int water = 3000; // миллилитров воды
+        int water = 6540; // миллилитров воды
         int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        int chicken = 5; // куриных бёдер
+        int spices = 18; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
